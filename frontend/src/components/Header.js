6f7953300f125761f5e924baa45cd55ff6eb4fbf
Bohdan Bilyk <bohdanbilykwork@gmail.com>
2021-01-20 10:27:23 +0200

    add routing with react-router-dom && react-router-bootstrap

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,26 @@
+import { LinkContainer } from "react-router-bootstrap";
 import { Navbar, Nav, Container } from "react-bootstrap";
 
 export const Header = () => (
   <header>
     <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
       <Container>
-        <Navbar.Brand href="/">Shop</Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand>Shop</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="/cart">
-              <i className="fas fa-shopping-cart"></i> Cart
-            </Nav.Link>
-            <Nav.Link href="/login">
-              <i className="fas fa-user"></i> Sign in
-            </Nav.Link>
+            <LinkContainer to="/cart">
+              <Nav.Link>
+                <i className="fas fa-shopping-cart"></i> Cart
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>
+                <i className="fas fa-user"></i> Sign in
+              </Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Container>
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,1 @@
+export const ProductScreen = () => <div>ProductScreen</div>;
