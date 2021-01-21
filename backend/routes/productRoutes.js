547294e5ab8f2e68c0d96dd7a8b932f769dd913e
Bohdan Bilyk <bohdanbilykwork@gmail.com>
2021-01-21 17:47:30 +0200

    add custom error handling middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -12,6 +12,7 @@ router.get(
   "/",
   asyncHandler(async (req, res) => {
     const products = await Product.find({});
+
     res.json(products);
   })
 );
@@ -27,10 +28,9 @@ router.get(
     if (product) {
       res.json(product);
     } else {
-      res.status(404).json({ message: "Product not found" });
+      res.status(404);
+      throw new Error("Product not found");
     }
-
-    res.json(product);
   })
 );
 
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import colors from "colors";
 
 import { connectDB } from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 import productRoutes from "./routes/productRoutes.js";
 
@@ -15,6 +16,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(
   PORT,
