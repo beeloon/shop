318c1836db013592c6e2ae2990472822d305fea7
Bohdan Bilyk <bohdanbilykwork@gmail.com>
2021-01-24 10:41:53 +0200

    add savePaymentMethod action

--- frontend/src/storage/cart/reducer.js
+++ frontend/src/storage/cart/reducer.js
@@ -2,6 +2,7 @@ import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
   CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD,
 } from "./constants";
 
 const addItemToCart = (state, payload) => {
@@ -34,6 +35,8 @@ export const cart = (
       return removeItemFromCart(state, action.payload);
     case CART_SAVE_SHIPPING_ADDRESS:
       return { ...state, shippingAddress: action.payload };
+    case CART_SAVE_PAYMENT_METHOD:
+      return { ...state, paymentMethod: action.payload };
     default:
       return state;
   }
